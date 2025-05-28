f104bbd49153000593eec24549506a2193fc9766
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-05-28 18:40:57 +0800

    feat(form): add input field component

---
+++ components/ui/form/input-field.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Input, type InputProps } from "@/components/ui/input/input";
+import { Label } from "@/components/ui/label/label";
+import { useId } from "react";
+import { Alert } from "./alert";
+import { useFieldContext } from "./form";
+
+export type InputFieldProps = InputProps & {
+	label: string;
+};
+
+export function InputField({ label, required, ...props }: InputFieldProps) {
+	const id = useId();
+	const field = useFieldContext<string>();
+
+	return (
+		<div className="space-y-2">
+			<div className="relative">
+				<Input
+					className="peer placeholder:text-transparent"
+					id={`${id}-${field.name}`}
+					placeholder=" "
+					required={required}
+					name={field.name}
+					value={field.state.value}
+					onBlur={field.handleBlur}
+					onChange={(event) => field.handleChange(event.target.value)}
+					variant={
+						field.state.meta.isBlurred && !field.state.meta.isValid
+							? "error"
+							: "default"
+					}
+					{...props}
+				/>
+				<Label
+					htmlFor={id}
+					className="-top-1.5 peer-placeholder-shown:-translate-y-1/2 peer-focus:-top-1.5 absolute left-0 ml-2 cursor-text bg-background-100 pr-2 pl-2 text-gray-1000 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-gray-900 peer-placeholder-shown:text-sm peer-focus:translate-y-0 peer-focus:bg-background-100 peer-focus:text-gray-1000 peer-focus:text-xs"
+				>
+					{label}
+					{required && <span className="ml-1 text-red-700">*</span>}
+				</Label>
+			</div>
+			<Alert meta={field.state.meta} />
+		</div>
+	);
+}
