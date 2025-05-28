21a645e4b435beedab6e795fd01484c9a20b3353
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-05-28 16:22:51 +0800

    feat(ui): add toast component

---
+++ components/ui/toast/toast.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { cn } from "@/lib/cn";
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import { XIcon } from "lucide-react";
+import { toast as sonnerToast } from "sonner";
+
+export type ToastProps = VariantProps<typeof toastVariant> & {
+	id: string | number;
+	title?: string;
+	description: string;
+	variant?: "default" | "success" | "warning" | "error";
+};
+
+export const toastVariant = cva(
+	"flex w-full cursor-pointer items-center justify-between rounded-xl border border-gray-400 px-4 py-3 shadow-md focus-visible:ring-2 focus-visible:ring-neutral-700 md:w-96",
+	{
+		variants: {
+			variant: {
+				default: "bg-gray-100 text-gray-1000",
+				success: "bg-blue-800 text-light",
+				warning: "bg-amber-800 text-dark",
+				error: "bg-red-800 text-light",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+		},
+	},
+);
+
+export function toast(toast: Omit<ToastProps, "id">) {
+	return sonnerToast.custom((id) => (
+		<Toast
+			id={id}
+			title={toast.title}
+			description={toast.description}
+			variant={toast.variant}
+		/>
+	));
+}
+
+export function Toast({ description, id, title, variant }: ToastProps) {
+	return (
+		<div className={cn(toastVariant({ variant }))}>
+			<div className="flex w-full items-center justify-between gap-1">
+				<div className="w-full">
+					{title && <p className="font-bold tracking-tighter">{title}</p>}
+					<p className="text-pretty text-sm">{description}</p>
+				</div>
+			</div>
+			<button
+				type="button"
+				className={cn(
+					"flex size-8 shrink-0 cursor-pointer items-center justify-center rounded-lg transition-all hover:bg-gray-300 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-600",
+					variant === "success" && "hover:bg-blue-700/80",
+					variant === "warning" && "hover:bg-amber-700/80",
+					variant === "error" && "hover:bg-red-700/80",
+				)}
+				onClick={() => {
+					sonnerToast.dismiss(id);
+				}}
+			>
+				<XIcon size={20} />
+			</button>
+		</div>
+	);
+}
