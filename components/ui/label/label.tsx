5cf50cffddf51b24a000faefdaa4e6bcd748241b
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-05-28 16:10:19 +0800

    feat(ui): add label component

---
+++ components/ui/label/label.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/cn";
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import { Label as LabelPrimitives } from "radix-ui";
+import type { ComponentProps } from "react";
+
+export type LabelProps = ComponentProps<typeof LabelPrimitives.Root> &
+	VariantProps<typeof labelVariants>;
+
+export const labelVariants = cva(
+	"font-medium text-neutral-100 text-sm leading-none peer-disabled:cursor-not-allowed",
+	{
+		variants: {
+			variant: {
+				error: "text-error-500",
+			},
+			position: {
+				inset: "absolute top-3 left-4 z-50 text-xs transition-all",
+			},
+		},
+	},
+);
+
+export function Label({
+	className,
+	variant,
+	position,
+	ref,
+	...props
+}: LabelProps) {
+	return (
+		<LabelPrimitives.Root
+			ref={ref}
+			className={cn(labelVariants({ variant, position, className }))}
+			{...props}
+		/>
+	);
+}
+Label.displayName = LabelPrimitives.Root.displayName;
