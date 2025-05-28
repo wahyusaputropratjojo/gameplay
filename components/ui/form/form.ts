c133e752785354bd4cd5a35c75bfd454d90be5bc
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-05-28 19:05:16 +0800

    feat(form): implement field component into form hook

--- components/ui/form/form.ts
+++ components/ui/form/form.ts
@@ -1,4 +1,7 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+import { InputField } from "./input-field";
+import { InputPasswordField } from "./input-password-field";
+import { SubmitButton } from "./submit-button";
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
 	createFormHookContexts();
@@ -6,6 +9,11 @@ export const { fieldContext, formContext, useFieldContext, useFormContext } =
 export const { useAppForm, withForm } = createFormHook({
 	fieldContext,
 	formContext,
-	fieldComponents: {},
-	formComponents: {},
+	fieldComponents: {
+		InputField,
+		InputPasswordField,
+	},
+	formComponents: {
+		SubmitButton,
+	},
 });
