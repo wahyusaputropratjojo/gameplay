d955f8807f37275051e6ff6f4e2856bd6aa80507
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-05-29 21:39:05 +0800

    feat(form): create field level schema validation for sign in form

--- app/sign-in/_form/schema.ts
+++ app/sign-in/_form/schema.ts
@@ -10,3 +10,15 @@ export const signInFormSchema = z.object({
 		),
 	password: z.string().min(1, { error: "Password is required." }),
 });
+
+export const emailSchema = z
+	.string()
+	.min(1, { error: "Email is required." })
+	.regex(
+		/^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i,
+		{ error: "Please enter a valid email address." },
+	);
+
+export const passwordSchema = z
+	.string()
+	.min(1, { error: "Password is required." });
