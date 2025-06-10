151a886f64b698dd1100771ae3605a1cb8a76502
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-06-10 22:55:39 +0800

    feat(db): setup Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "@/lib/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	out: "./db/migration",
+	schema: "./db/schema",
+	dialect: "postgresql",
+	casing: "snake_case",
+	dbCredentials: {
+		url: process.env.DB_URL as string,
+	},
+});
