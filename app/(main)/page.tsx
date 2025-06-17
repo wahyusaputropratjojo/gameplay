1ef57809498193a3e9d51aac59105adf73624620
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-06-17 19:16:43 +0800

    feat(page): move home page to implement main layout

---
+++ app/(main)/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+	return <main className="container flex min-h-dvh flex-col gap-6">Home.</main>;
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main className="flex min-h-screen items-center justify-center">
-			<h1>Gameplay.</h1>
-		</main>
-	);
-}
