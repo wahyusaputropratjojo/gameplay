e534ef4df8a5403bb0bab0b6cdf512d6167304e9
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-06-24 20:29:56 +0800

    feat(component): create dot button component for Embla Carousel

---
+++ components/carousel/dot-button.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/cn";
+import type { ComponentProps } from "react";
+
+type DotButtonProps = {
+	index: number;
+	selectedIndex: number;
+} & ComponentProps<"button">;
+
+export function DotButton({
+	index,
+	selectedIndex,
+	children,
+	...props
+}: DotButtonProps) {
+	return (
+		<button
+			type="button"
+			{...props}
+		>
+			<div
+				className={cn(
+					"size-1.5 rounded-full bg-gray-500 lg:size-2.5",
+					index === selectedIndex && "bg-gray-1000",
+				)}
+			/>
+		</button>
+	);
+}
