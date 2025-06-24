3cdc397b2373ef6299e841eeb2eb75a3d70f83b0
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-06-24 20:32:07 +0800

    feat(component): create header component

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import { GameplayLogo } from "@/components/logo/gameplay-logo";
+
+export function Header() {
+	return (
+		<header className="container bg-background-100 py-4">
+			<section className="flex justify-between">
+				<GameplayLogo />
+			</section>
+		</header>
+	);
+}
