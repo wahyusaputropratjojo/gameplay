8378518b7193bc73ba539ebcb94810513094e8d0
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-06-24 20:43:44 +0800

    feat(component): create shelf component

---
+++ components/shelf.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { GridCard } from "@/components/card/grid-card";
+import { NextButton } from "@/components/carousel/next-button";
+import { PrevButton } from "@/components/carousel/prev-button";
+import { usePrevNextButton } from "@/hooks/carousel/use-prev-next-button";
+import useEmblaCarousel from "embla-carousel-react";
+
+type ShelfProps = {
+	title: string;
+	items: {
+		id: string;
+		title: string;
+		gridURL: string;
+	}[];
+};
+
+export function Shelf({ title, items }: ShelfProps) {
+	const [emblaRef, emblaAPI] = useEmblaCarousel({
+		slidesToScroll: "auto",
+		breakpoints: {
+			"(min-width: 1280px)": {
+				watchDrag: false,
+			},
+		},
+	});
+
+	const {
+		onNextButtonClick,
+		onPrevButtonClick,
+		prevButtonDisabled,
+		nextButtonDisabled,
+	} = usePrevNextButton(emblaAPI);
+
+	return (
+		<section className="flex flex-col gap-2">
+			<div className="flex justify-between">
+				<h3>{title}</h3>
+				<div className="space-x-2">
+					<PrevButton
+						onClick={onPrevButtonClick}
+						disabled={prevButtonDisabled}
+						className="rounded-full"
+					/>
+					<NextButton
+						onClick={onNextButtonClick}
+						disabled={nextButtonDisabled}
+						className="rounded-full"
+					/>
+				</div>
+			</div>
+			<div
+				className="overflow-hidden"
+				ref={emblaRef}
+			>
+				<div className="-ml-4 flex">
+					{items.map((item) => (
+						<GridCard
+							key={item.id}
+							src={item.gridURL}
+							alt={`${item.title} - Grid`}
+							title={item.title}
+							className="shrink-0 grow-0 basis-6/10 pl-4 md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+}
