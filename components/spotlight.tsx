f65a45cd9b6de52647e50f26bbb39eafa80b5fb9
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-07-17 21:38:53 +0800

    feat(component): update spotlight component

--- components/spotlight.tsx
+++ components/spotlight.tsx
@@ -1,22 +1,20 @@
 "use client";
 
-import { DotButton } from "@/components/carousel/dot-button";
-import { useDotButton } from "@/hooks/carousel/use-dot-button";
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 
 type SpotlightProps = {
-	id: string;
-	title: string;
-	description: string;
-	heroURL: string;
-	logoURL: string;
-}[];
-
-export function Spotlight({ items }: { items: SpotlightProps }) {
-	const [emblaRef, emblaAPI] = useEmblaCarousel(
+	items: {
+		id: string;
+		title: string;
+		description: string;
+		heroURL: string;
+	}[];
+};
+export function Spotlight({ items }: SpotlightProps) {
+	const [emblaRef] = useEmblaCarousel(
 		{
 			breakpoints: {
 				"(min-width: 1280px)": {
@@ -39,9 +37,6 @@ export function Spotlight({ items }: { items: SpotlightProps }) {
 		],
 	);
 
-	const { onDotButtonClick, selectedIndex, scrollSnaps } =
-		useDotButton(emblaAPI);
-
 	return (
 		<section
 			className="flex flex-col gap-2 overflow-hidden"
@@ -52,7 +47,7 @@ export function Spotlight({ items }: { items: SpotlightProps }) {
 					items.map((item) => (
 						<div
 							key={item.id}
-							className="relative aspect-standard-vertical select-none overflow-hidden rounded-xl md:aspect-standard-horizontal xl:aspect-anamorphic-horizontal"
+							className="relative aspect-standard-vertical cursor-pointer select-none overflow-hidden rounded-xl md:aspect-standard-horizontal xl:aspect-anamorphic-horizontal"
 						>
 							<div className="absolute bottom-0 z-20 space-y-2 p-6">
 								<h3 className="text-gray-100 dark:text-gray-1000">
@@ -66,8 +61,9 @@ export function Spotlight({ items }: { items: SpotlightProps }) {
 							<Image
 								src={item.heroURL}
 								alt={`${item.title} - Hero`}
+								priority={items.indexOf(item) === 0}
 								draggable="false"
-								className="z-0 size-full object-cover"
+								className="z-0 size-full object-cover object-center"
 								sizes="(min-width: 1540px) 1841px, (min-width: 1280px) calc(9.17vw + 1391px), (min-width: 1040px) 1769px, (min-width: 780px) 1367px, (min-width: 640px) calc(45.83vw + 1677px), calc(349.69vw - 158px)"
 								placeholder="blur"
 								blurDataURL="data:text/plain;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOU+g8AATkBG5//A4sAAAAASUVORK5CYII="
@@ -76,18 +72,6 @@ export function Spotlight({ items }: { items: SpotlightProps }) {
 							/>
 						</div>
 					))}
-			</div>
-
-			<div className="flex size-full w-min items-center justify-center space-x-1.5 self-center rounded-full p-1.5 lg:space-x-2">
-				{scrollSnaps.map((id, index) => (
-					<DotButton
-						key={id}
-						index={index}
-						selectedIndex={selectedIndex}
-						onClick={() => onDotButtonClick(index)}
-						className="cursor-pointer"
-					/>
-				))}
 			</div>
 		</section>
 	);
