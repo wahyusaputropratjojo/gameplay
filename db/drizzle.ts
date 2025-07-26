5e4ef1dbf6f4026035fe86fa31c8f78dbd664f9d
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-07-26 17:29:57 +0800

    feat(db): update Drizzle ORM setup

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,11 @@
 import "@/lib/env";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "@/db/schema";
 
 export const db = drizzle({
-	connection: process.env.DB_URL as string,
+	connection: {
+		connectionString: process.env.DB_URL as string,
+	},
+	schema: schema,
+	casing: "snake_case",
 });
