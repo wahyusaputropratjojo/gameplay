533c29a6968118b6f3469007c5e916e797f0cf19
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-07-30 22:35:29 +0800

    feat(queries): add query function for new releases shelf games

---
+++ lib/queries/get-new-releases-shelf-games.ts
@@ -0,0 +1,21 @@
+import "server-only";
+import { eq, sql } from "drizzle-orm";
+import { cache } from "react";
+import { db } from "@/db/drizzle";
+import { game, gameMedia, media, mediaType } from "@/db/schema";
+
+export const getNewReleasesShelfGames = cache(async () => {
+	return db
+		.select({
+			id: game.id,
+			name: game.name,
+			slug: game.slug,
+			gridURL: sql<string>`MAX(CASE WHEN ${mediaType.name} = 'Grid' THEN ${media.url} END)`,
+		})
+		.from(game)
+		.innerJoin(gameMedia, eq(gameMedia.gameId, game.id))
+		.innerJoin(media, eq(media.id, gameMedia.mediaId))
+		.innerJoin(mediaType, eq(mediaType.id, media.mediaTypeId))
+		.groupBy(game.id)
+		.limit(20);
+});
