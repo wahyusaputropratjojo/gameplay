91592851f7f9081f94947627e34e32fcc44f7c0d
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-07-30 22:32:20 +0800

    feat(queries): add query function for upcoming spotlight games

---
+++ lib/queries/get-upcoming-spotlight-games.ts
@@ -0,0 +1,24 @@
+import "server-only";
+import { desc, eq, sql } from "drizzle-orm";
+import { cache } from "react";
+import { db } from "@/db/drizzle";
+import { game, gameMedia, gamePlatform, media, mediaType } from "@/db/schema";
+
+export const getUpcomingSpotlightGames = cache(async () => {
+	return db
+		.select({
+			id: game.id,
+			name: game.name,
+			description: game.description,
+			slug: game.slug,
+			heroURL: sql<string>`MAX(CASE WHEN ${mediaType.name} = 'Hero' THEN ${media.url} END)`,
+		})
+		.from(game)
+		.innerJoin(gameMedia, eq(game.id, gameMedia.gameId))
+		.innerJoin(media, eq(media.id, gameMedia.mediaId))
+		.innerJoin(mediaType, eq(mediaType.id, media.mediaTypeId))
+		.innerJoin(gamePlatform, eq(gamePlatform.gameId, game.id))
+		.groupBy(game.id)
+		.orderBy(desc(sql`MIN(${gamePlatform.releaseDate})`))
+		.limit(5);
+});
