29eee09f60fb94d3f47f81d90b582f6083b3e4b4
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-08-07 16:37:13 +0800

    feat: setup Payload CMS with initial configuration

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from '@payloadcms/next/withPayload';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -11,4 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
---
+++ payload.config.ts
@@ -0,0 +1,44 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { payloadCloudPlugin } from '@payloadcms/payload-cloud';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import { buildConfig } from 'payload';
+import sharp from 'sharp';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    importMap: {
+      baseDir: path.resolve(dirname),
+      importMapFile: path.resolve(dirname, 'app', '(payload)', 'import-map.ts'),
+    },
+    meta: {
+      title: 'Administrator - Gameplay',
+      description: 'Collection of video game information.',
+      icons: [
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          url: '/favicon.ico',
+        },
+      ],
+    },
+  },
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'lib', 'types', 'payload.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
+    },
+    idType: 'uuid',
+  }),
+  sharp,
+  telemetry: false,
+  plugins: [payloadCloudPlugin()],
+});
