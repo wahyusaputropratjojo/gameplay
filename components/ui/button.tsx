23ec98bfc5f5e4e5f444d4d568e1865e841c1473
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-08-07 15:52:44 +0800

    refactor: change package source for slot in button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { Slot } from '@radix-ui/react-slot';
+import { Slot } from 'radix-ui';
 import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 import type { ComponentProps } from 'react';
