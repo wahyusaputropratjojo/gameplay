63db7b74fbd11366002b623be809de0b6263daf8
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-02 21:44:02 +0800

    refactor: rename and move globals.css to style.css

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -2,25 +2,25 @@ import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import '../globals.css';
+import './style.css';
 
 export const metadata: Metadata = {
   description: 'Collection of video game information.',
   title: 'Gameplay',
 };
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+      data-theme="dark"
+      lang="en"
+    >
+    <body>{children}</body>
     </html>
   );
 }
