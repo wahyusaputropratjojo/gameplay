9686a977048aee5f4de162a17d3fc4a9e43cdfd6
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-15 20:40:19 +0800

    chore(payload): update payload config

--- payload.config.ts
+++ payload.config.ts
@@ -9,17 +9,24 @@ import { buildConfig } from 'payload';
 import sharp from 'sharp';
 import { ageRating } from '@/lib/collections/age-rating';
 import { ageRatingAuthority } from '@/lib/collections/age-rating-authority';
+import { ageRatingLogo } from '@/lib/collections/age-rating-logo';
 import { company } from '@/lib/collections/company';
+import { companyLogo } from '@/lib/collections/company-logo';
 import { continent } from '@/lib/collections/continent';
 import { country } from '@/lib/collections/country';
+import { countryFlag } from '@/lib/collections/country-flag';
 import { game } from '@/lib/collections/game';
+import { gameGrid } from '@/lib/collections/game-grid';
+import { gameHero } from '@/lib/collections/game-hero';
+import { gameLogo } from '@/lib/collections/game-logo';
 import { gamePlatform } from '@/lib/collections/game-platform';
 import { gameStore } from '@/lib/collections/game-store';
 import { genre } from '@/lib/collections/genre';
-import { media } from '@/lib/collections/media';
 import { platform } from '@/lib/collections/platform';
+import { platformLogo } from '@/lib/collections/platform-logo';
 import { platformType } from '@/lib/collections/platform-type';
 import { store } from '@/lib/collections/store';
+import { storeLogo } from '@/lib/collections/store-logo';
 import { theme } from '@/lib/collections/theme';
 import { user } from '@/lib/collections/user';
 
@@ -36,6 +43,7 @@ export default buildConfig({
             prefillOnly: true,
           }
         : false,
+    avatar: 'default',
     importMap: {
       baseDir: path.resolve(dirname),
       importMapFile: path.resolve(dirname, 'app', '(payload)', 'import-map.ts'),
@@ -62,6 +70,7 @@ export default buildConfig({
   },
   collections: [
     ageRating,
+    ageRatingLogo,
     ageRatingAuthority,
     company,
     continent,
@@ -70,16 +79,24 @@ export default buildConfig({
     gamePlatform,
     gameStore,
     genre,
-    media,
     platform,
     platformType,
     store,
     theme,
+    gameHero,
     user,
+    gameGrid,
+    gameLogo,
+    companyLogo,
+    countryFlag,
+    platformLogo,
+    storeLogo,
   ],
   db: postgresAdapter({
     allowIDOnCreate: true,
+    generateSchemaOutputFile: path.resolve(dirname, 'lib/db/schema.ts'),
     idType: 'uuid',
+
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
@@ -91,6 +108,7 @@ export default buildConfig({
     ],
   }),
   secret: process.env.PAYLOAD_SECRET || '',
+  serverURL: process.env.NEXT_PUBLIC_URL,
   sharp,
   telemetry: false,
   typescript: {
