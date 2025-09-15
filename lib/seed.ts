2f9b951950745642e459976b3420e718525c8bdc
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-15 20:34:09 +0800

    feat(payload): update seed function

--- lib/seed.ts
+++ lib/seed.ts
@@ -1,21 +1,30 @@
 import { ageRatingSeed } from '@/lib/seeds/age-rating';
 import { ageRatingAuthoritySeed } from '@/lib/seeds/age-rating-authority';
+import { ageRatingLogoSeed } from '@/lib/seeds/age-rating-logo';
 import { companySeed } from '@/lib/seeds/company';
+import { companyLogoSeed } from '@/lib/seeds/company-logo';
 import { continentSeed } from '@/lib/seeds/continent';
 import { countrySeed } from '@/lib/seeds/country';
+import { countryFlagSeed } from '@/lib/seeds/country-flag';
 import { genreSeed } from '@/lib/seeds/genre';
-import { mediaSeed } from '@/lib/seeds/media';
 import { platformSeed } from '@/lib/seeds/platform';
 import { platformTypeSeed } from '@/lib/seeds/platform-type';
 import { reset } from '@/lib/seeds/reset';
 import { storeSeed } from '@/lib/seeds/store';
+import { storeLogoSeed } from '@/lib/seeds/store-logo';
 import { themeSeed } from '@/lib/seeds/theme';
 import { userSeed } from '@/lib/seeds/user';
 
 async function seed() {
   await reset();
   await userSeed();
-  await mediaSeed();
+
+  // Media
+  await ageRatingLogoSeed();
+  await companyLogoSeed();
+  await countryFlagSeed();
+  await storeLogoSeed();
+
   await continentSeed();
   await countrySeed();
   await companySeed();
