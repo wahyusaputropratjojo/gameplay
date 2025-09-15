ccc07754205ce571d199ea67abaf8a19eac328bd
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-15 20:18:56 +0800

    feat(payload): create hook that create blur data for image

---
+++ lib/collections/hooks/create-blur-data.ts
@@ -0,0 +1,41 @@
+import type { CollectionBeforeChangeHook } from 'payload';
+import sharp, { type BlurOptions, type FormatEnum } from 'sharp';
+
+type BlurDataOptions = {
+  format?: keyof FormatEnum;
+  blur?: BlurOptions;
+  fieldName: string;
+};
+
+export function createBlurData({
+  format = 'webp',
+  blur = { sigma: 2 },
+  fieldName,
+}: BlurDataOptions): CollectionBeforeChangeHook {
+  return async ({ data, req }) => {
+    if (!req.file) {
+      return data;
+    }
+
+    try {
+      const imageBuffer = req.file.data;
+
+      const placeholderBuffer = await sharp(imageBuffer)
+        .resize(20)
+        .blur(blur)
+        .toFormat(format)
+        .toBuffer();
+
+      const base64 = placeholderBuffer.toString('base64');
+      const dataURI = `data:image/${format};base64,${base64}`;
+
+      return {
+        ...data,
+        [fieldName]: dataURI,
+      };
+    } catch (error) {
+      req.payload.logger.error(`Error generating Blur Data: ${error}`);
+      return data;
+    }
+  };
+}
