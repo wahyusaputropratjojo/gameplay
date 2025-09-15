b604e9e15a25035303f5a77489c09907af151be0
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-15 20:04:13 +0800

    feat(components): update spotlight component props type

--- components/spotlight.tsx
+++ components/spotlight.tsx
@@ -12,9 +12,21 @@ type SpotlightProps = {
     name: string;
     description: string;
     slug: string;
-    heroURL: string;
+    hero: {
+      alt: string;
+      url: string;
+      width: number;
+      height: number;
+    };
+    logo: {
+      alt: string;
+      url: string;
+      width: number;
+      height: number;
+    };
   }[];
 };
+
 export function Spotlight({ items }: SpotlightProps) {
   const [emblaRef] = useEmblaCarousel(
     {
@@ -45,28 +57,37 @@ export function Spotlight({ items }: SpotlightProps) {
         {items.length > 0 &&
           items.map((item) => (
             <Link href={`/game/${item.slug}`} key={item.id}>
-              <div className="relative aspect-standard-vertical cursor-pointer select-none overflow-hidden rounded-xl md:aspect-standard-horizontal xl:aspect-anamorphic-horizontal">
-                <div className="absolute bottom-0 z-20 space-y-2 p-6">
-                  <h3 className="text-gray-100 dark:text-gray-1000">
-                    {item.name}
-                  </h3>
-                  <p className="line-clamp-3 max-w-[65ch] text-gray-100 dark:text-gray-1000">
-                    {item.description}
-                  </p>
-                </div>
-                <div className="absolute z-10 h-full w-full bg-linear-to-t from-20% from-dark/80 to-transparent" />
+              <div className="grid aspect-standard-vertical cursor-pointer select-none grid-cols-1 grid-rows-1 overflow-hidden rounded-xl md:aspect-standard-horizontal xl:aspect-anamorphic-horizontal">
                 <Image
-                  alt={`${item.name} - Hero`}
+                  alt={item.hero.alt}
                   blurDataURL="data:text/plain;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOU+g8AATkBG5//A4sAAAAASUVORK5CYII="
-                  className="z-0 size-full object-cover object-center"
+                  className="col-span-full row-span-full size-full object-cover"
                   draggable="false"
-                  height={620}
+                  height={item.hero.height}
                   placeholder="blur"
-                  priority={items.indexOf(item) === 0}
+                  priority={true}
                   sizes="(min-width: 1540px) 1841px, (min-width: 1280px) calc(9.17vw + 1391px), (min-width: 1040px) 1769px, (min-width: 780px) 1367px, (min-width: 640px) calc(45.83vw + 1677px), calc(349.69vw - 158px)"
-                  src={item.heroURL}
-                  width={1920}
+                  src={item.hero.url}
+                  width={item.hero.width}
                 />
+
+                <div className="z-10 col-span-full row-span-full bg-gradient-to-t from-30% from-dark/80 to-70% to-transparent" />
+
+                <div className="z-20 col-span-full row-span-full flex size-full items-end space-y-2 self-end p-6 xl:p-12">
+                  <div className="space-y-2 lg:space-y-6">
+                    <Image
+                      alt={item.logo.alt}
+                      className="h-48 w-8/12 object-contain object-bottom-left md:w-6/12 lg:h-72 xl:w-4/12"
+                      height={item.logo.height}
+                      priority={true}
+                      src={item.logo.url}
+                      width={item.logo.width}
+                    />
+                    <p className="line-clamp-3 max-w-[65ch] text-gray-100 dark:text-gray-1000">
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
               </div>
             </Link>
           ))}
