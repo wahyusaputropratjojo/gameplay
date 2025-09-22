0fab70a7e05bd9f4a5334292b04f640c8233e17a
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-22 16:29:12 +0800

    feat(component): improve grid card component prop types and add type guard to render the image

--- components/card/grid-card.tsx
+++ components/card/grid-card.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import { cn } from '@/lib/cn';
+import type { GameGrid } from '@/lib/types/payload';
 
 type GridCardProps = {
-  src: string;
-  alt: string;
+  image: GameGrid;
   name: string;
   className?: string;
 };
 
-export function GridCard({ className, src, alt, name }: GridCardProps) {
+export function GridCard({ className, image, name }: GridCardProps) {
   return (
     <div
       className={cn(
@@ -16,20 +16,25 @@ export function GridCard({ className, src, alt, name }: GridCardProps) {
         className
       )}
     >
-      <div className="relative">
-        <div className="absolute z-20 size-full rounded-xl bg-light/10 opacity-0 transition-opacity group-hover:opacity-100" />
-        <Image
-          alt={alt}
-          blurDataURL="data:text/plain;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOU+g8AATkBG5//A4sAAAAASUVORK5CYII="
-          className="w-full rounded-xl"
-          draggable="false"
-          height={450}
-          placeholder="blur"
-          sizes="(min-width: 1320px) 300px, (min-width: 640px) calc(2.88vw + 263px), calc(50vw - 23px)"
-          src={src}
-          width={300}
-        />
-      </div>
+      {typeof image.blurData === 'string' &&
+        typeof image.height === 'number' &&
+        typeof image.url === 'string' &&
+        typeof image.width === 'number' && (
+          <div className="relative">
+            <div className="absolute z-20 size-full rounded-xl bg-light/10 opacity-0 transition-opacity group-hover:opacity-100" />
+            <Image
+              alt={image.alt}
+              blurDataURL={image.blurData}
+              className="w-full rounded-xl"
+              draggable="false"
+              height={image.height}
+              placeholder="blur"
+              sizes="(min-width: 1320px) 300px, (min-width: 640px) calc(2.88vw + 263px), calc(50vw - 23px)"
+              src={image.url}
+              width={image.width}
+            />
+          </div>
+        )}
       <div>
         <p className="line-clamp-1 font-medium text-gray-700 text-sm">
           Base Game
