e2819e2df97972bf15d09678a0d1ff7cdfd059df
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-23 21:09:58 +0800

    feat(component): reworked highlight card with better prop types and flexible image

--- components/card/highlight-card.tsx
+++ components/card/highlight-card.tsx
@@ -1,42 +1,25 @@
-import Image from 'next/image';
-import { cn } from '@/lib/cn';
+import Image, { type ImageProps } from 'next/image';
 
 type HighlightCardProps = {
-  src: string;
-  alt: string;
-  name: string;
+  title: string;
   description: string;
-  className?: string;
+  image: Omit<ImageProps, 'className'>;
 };
 
 export function HighlightCard({
-  className,
-  src,
-  alt,
-  name,
+  title,
   description,
+  image,
 }: HighlightCardProps) {
   return (
-    <div
-      className={cn(
-        'group select-none space-y-2 hover:cursor-pointer',
-        className
-      )}
-    >
+    <div className="group select-none space-y-2 hover:cursor-pointer">
       <div className="relative">
         <div className="absolute z-20 size-full rounded-xl bg-light/10 opacity-0 transition-opacity group-hover:opacity-100" />
-        <Image
-          alt={alt}
-          blurDataURL="data:text/plain;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOU+g8AATkBG5//A4sAAAAASUVORK5CYII="
-          className="aspect-wide-horizontal w-full rounded-xl object-cover"
-          height="720"
-          placeholder="blur"
-          src={src}
-          width="1280"
-        />
+        <Image className="rounded-xl" {...image} />
       </div>
+
       <div>
-        <p className="line-clamp-1 font-semibold text-lg">{name}</p>
+        <p className="line-clamp-1 font-semibold text-lg">{title}</p>
         <p className="line-clamp-2 max-w-[35ch] text-gray-700 text-sm">
           {description}
         </p>
