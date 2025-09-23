901463494e4861294b6cb5587182c6ddda23c8f6
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-23 21:29:34 +0800

    feat(component): implement content section component in shelf component

--- components/shelf.tsx
+++ components/shelf.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { GridCard } from '@/components/card/grid-card';
 import { NextButton } from '@/components/carousel/next-button';
 import { PrevButton } from '@/components/carousel/prev-button';
+import { ContentSection } from '@/components/content-section';
 import { usePrevNextButton } from '@/hooks/carousel/use-prev-next-button';
 import type { Game } from '@/lib/types/payload';
 
@@ -33,21 +34,18 @@ export function Shelf({ title, items }: ShelfProps) {
   } = usePrevNextButton(emblaAPI);
 
   return (
-    <section className="flex flex-col gap-2">
-      <div className="flex justify-between">
-        <h3>{title}</h3>
-        <div className="space-x-2">
-          <PrevButton
-            className="rounded-full"
-            disabled={prevButtonDisabled}
-            onClick={onPrevButtonClick}
-          />
-          <NextButton
-            className="rounded-full"
-            disabled={nextButtonDisabled}
-            onClick={onNextButtonClick}
-          />
-        </div>
+    <ContentSection title={title}>
+      <div className="absolute hidden space-x-2 justify-self-end lg:block">
+        <PrevButton
+          className="rounded-full"
+          disabled={prevButtonDisabled}
+          onClick={onPrevButtonClick}
+        />
+        <NextButton
+          className="rounded-full"
+          disabled={nextButtonDisabled}
+          onClick={onNextButtonClick}
+        />
       </div>
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="-ml-4 flex">
@@ -64,6 +62,6 @@ export function Shelf({ title, items }: ShelfProps) {
           ))}
         </div>
       </div>
-    </section>
+    </ContentSection>
   );
 }
