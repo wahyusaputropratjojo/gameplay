9ea87bc247a98c592d48c5f1067a46a9e316180a
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-23 21:12:38 +0800

    feat(component): improve prop types,  implement new content section and highlight card in discovery component

--- components/discovery.tsx
+++ components/discovery.tsx
@@ -1,40 +1,45 @@
 import Link from 'next/link';
 import { HighlightCard } from '@/components/card/highlight-card';
+import { ContentSection } from '@/components/content-section';
+import type { Game } from '@/lib/types/payload';
+
+type PickedGame = Pick<Game, 'id' | 'name' | 'slug' | 'description' | 'hero'>;
 
 type DiscoveryProps = {
   title?: string;
-  items: {
-    id: string;
-    name: string;
-    slug: string;
-    description: string;
-    hero: {
-      alt: string;
-      url: string;
-    };
-  }[];
+  games: PickedGame[];
 };
 
-export function Discovery({ title, items }: DiscoveryProps) {
+export function Discovery({ title, games }: DiscoveryProps) {
   return (
-    <div className="flex flex-col gap-2">
-      {title && <h3>{title}</h3>}
+    <ContentSection title={title}>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-4 lg:grid-cols-3">
-        {items.map((item) => (
+        {games.map((game) => (
           <Link
             className="last:hidden lg:last:block"
-            href={`/game/${item.slug}`}
-            key={item.id}
+            href={`/game/${game.slug}`}
+            key={game.id}
           >
-            <HighlightCard
-              alt={item.hero.alt}
-              description={item.description}
-              name={item.name}
-              src={item.hero.url}
-            />
+            {typeof game.hero === 'object' &&
+              typeof game.hero.sizes === 'object' &&
+              typeof game.hero.sizes.wide === 'object' &&
+              typeof game.hero.sizes.wide.url === 'string' &&
+              typeof game.hero.sizes.wide.height === 'number' &&
+              typeof game.hero.sizes.wide.width === 'number' && (
+                <HighlightCard
+                  description={game.description}
+                  image={{
+                    alt: game.hero.alt,
+                    height: game.hero.sizes.wide.height,
+                    src: game.hero.sizes.wide.url,
+                    width: game.hero.sizes.wide.width,
+                  }}
+                  title={game.name}
+                />
+              )}
           </Link>
         ))}
       </div>
-    </div>
+    </ContentSection>
   );
 }
