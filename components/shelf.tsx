88204e5b543d75faeab497d4b7dd21978995d2e1
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-23 21:50:46 +0800

    feat(component): change prop types and implement new grid card in shelf component

--- components/shelf.tsx
+++ components/shelf.tsx
@@ -13,10 +13,10 @@ type PickedGame = Pick<Game, 'id' | 'name' | 'slug' | 'grid'>;
 
 type ShelfProps = {
   title: string;
-  items: PickedGame[];
+  games: PickedGame[];
 };
 
-export function Shelf({ title, items }: ShelfProps) {
+export function Shelf({ title, games }: ShelfProps) {
   const [emblaRef, emblaAPI] = useEmblaCarousel({
     breakpoints: {
       '(min-width: 1280px)': {
@@ -49,15 +49,29 @@ export function Shelf({ title, items }: ShelfProps) {
       </div>
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="-ml-4 flex">
-          {items.map((item) => (
+          {games.map((game) => (
             <Link
               className="shrink-0 grow-0 basis-6/10 pl-4 md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
-              href={`/game/${item.slug}`}
-              key={item.id}
+              href={`/game/${game.slug}`}
+              key={game.id}
             >
-              {typeof item.grid === 'object' && (
-                <GridCard image={item.grid} name={item.name} />
-              )}
+              {typeof game.grid === 'object' &&
+                typeof game.grid.blurData === 'string' &&
+                typeof game.grid.height === 'number' &&
+                typeof game.grid.url === 'string' &&
+                typeof game.grid.width === 'number' && (
+                  <GridCard
+                    image={{
+                      alt: game.grid.alt,
+                      blurDataURL: game.grid.blurData,
+                      height: game.grid.height,
+                      placeholder: 'blur',
+                      src: game.grid.url,
+                      width: game.grid.width,
+                    }}
+                    title={game.name}
+                  />
+                )}
             </Link>
           ))}
         </div>
