9fa0b575b7378740ed189cdbd37325ff6318f3ea
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-24 15:10:34 +0800

    feat(section): create game publisher section for game page

---
+++ app/(frontend)/(main)/game/[slug]/game-publisher-section.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { EntityCard } from '@/components/card/entity-card';
+import { ContentSection } from '@/components/content-section';
+import type { Game } from '@/lib/types/payload';
+
+export function GamePublisherSection({ publisher }: Pick<Game, 'publisher'>) {
+  return (
+    <ContentSection
+      title="Publisher"
+      titleElement="heading-2"
+      titleSize="heading-4"
+    >
+      <div className="grid gap-2">
+        {publisher.map((item) => {
+          if (typeof item === 'object' && typeof item.logo === 'object') {
+            return (
+              <Link href={`/company/${item.slug}`} key={item.id}>
+                <EntityCard
+                  alt={item.logo.alt}
+                  fill
+                  name={item.name}
+                  padding="medium"
+                  src={item.logo.url as string}
+                />
+              </Link>
+            );
+          }
+          return 'Increase the Depth';
+        })}
+      </div>
+    </ContentSection>
+  );
+}
