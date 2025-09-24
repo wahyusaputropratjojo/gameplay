264056d00d8eee6e817a5c39b3879eeee8c3c147
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-24 14:38:50 +0800

    refactor: move from section directory back to game page directory

--- app/(frontend)/(main)/game/[slug]/page.tsx
+++ app/(frontend)/(main)/game/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import { BadgeListSection } from '@/components/section/badge-list-section';
-import { GameCompanyListSection } from '@/components/section/game-company-list-section';
-import { GameDurationSection } from '@/components/section/game-duration-section';
-import { GameImageSection } from '@/components/section/game-image-section';
-import { GameStoreSection } from '@/components/section/game-store-section';
 import { getGame } from '@/lib/queries/get-game';
+import { BadgeListSection } from './badge-list-section';
+import { GameCompanyListSection } from './game-company-list-section';
+import { GameDescriptionSection } from './game-description-section';
+import { GameDurationSection } from './game-duration-section';
+import { GameImageSection } from './game-image-section';
+import { GameStoreSection } from './game-store-section';
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -30,12 +31,9 @@ export default async function Page({ params }: Props) {
   }
 
   return (
-    <main className="relative space-y-8">
+    <div className="space-y-6">
       <GameImageSection hero={game.hero} logo={game.logo} />
-      <section className="space-y-2">
-        <h4>Description</h4>
-        <p>{game.description}</p>
-      </section>
+      <GameDescriptionSection description={game.description} />
       <div className="grid gap-8 md:grid-cols-2 md:gap-4">
         <BadgeListSection items={game.genre} title="Genre" />
         <BadgeListSection items={game.theme} title="Theme" />
@@ -48,6 +46,6 @@ export default async function Page({ params }: Props) {
       {game.storeLink && game.storeLink.length > 0 && (
         <GameStoreSection storeLink={game.storeLink} />
       )}
-    </main>
+    </div>
   );
 }
