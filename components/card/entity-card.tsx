d833021e35fefce31c233dd1fe47902a0cf7db1d
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-24 15:36:58 +0800

    feat(component): small change to potential issue in entity card component

--- components/card/entity-card.tsx
+++ components/card/entity-card.tsx
@@ -4,22 +4,20 @@ import { cn } from '@/lib/cn';
 export type EntityCardProps = {
   name: string;
   padding?: 'none' | 'small' | 'medium' | 'large';
-} & Partial<ImageProps>;
+} & ImageProps;
 
 export function EntityCard({
   name,
   padding = 'none',
   src,
-  alt,
   ...props
 }: EntityCardProps) {
   return (
     <div className="flex w-full items-center gap-4 rounded-(--card-radius) bg-gray-100 p-(--card-padding) transition-colors [--card-padding:--spacing(1.5)] [--card-radius:var(--radius-xl)] hover:bg-gray-200">
       <div className="relative size-18 shrink-0 overflow-hidden rounded-[calc(var(--card-radius)-var(--card-padding))] bg-light">
-        {src && alt && (
+        {src && (
           <Image
-            alt={alt}
-            className={cn({
+            className={cn('select-none', {
               'p-0': padding === 'none',
               'p-1': padding === 'small',
               'p-3': padding === 'medium',
@@ -30,7 +28,7 @@ export function EntityCard({
           />
         )}
       </div>
-      <p className="font-medium">{name}</p>
+      <p className="select-none font-medium">{name}</p>
     </div>
   );
 }
