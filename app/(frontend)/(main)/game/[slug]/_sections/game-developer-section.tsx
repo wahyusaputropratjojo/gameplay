eca62c01648bfbb8f682e8831c1cf841964e585e
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-25 19:46:35 +0800

    feat(section): implement unpopulated relationship field alert component in game developer section

--- app/(frontend)/(main)/game/[slug]/_sections/game-developer-section.tsx
+++ app/(frontend)/(main)/game/[slug]/_sections/game-developer-section.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import { UnpopulatedRelationshipFieldAlert } from '@/components/alert/unpopulated-relationship-field-alert';
 import { EntityCard } from '@/components/card/entity-card';
 import { ContentSection } from '@/components/content-section';
 import type { Game } from '@/lib/types/payload';
 
 export function GameDeveloperSection({ developer }: Pick<Game, 'developer'>) {
+  const isUnpopulated = developer.some((item) => typeof item === 'string');
+
   return (
     <ContentSection
       title="Developer"
@@ -11,22 +14,24 @@ export function GameDeveloperSection({ developer }: Pick<Game, 'developer'>) {
       titleSize="heading-4"
     >
       <div className="grid gap-2">
-        {developer.map((item) => {
-          if (typeof item === 'object' && typeof item.logo === 'object') {
-            return (
+        {developer.map(
+          (item) =>
+            typeof item === 'object' &&
+            typeof item.logo === 'object' &&
+            typeof item.logo.url === 'string' && (
               <Link href={`/company/${item.slug}`} key={item.id}>
                 <EntityCard
                   alt={item.logo.alt}
                   fill
                   name={item.name}
                   padding="medium"
-                  src={item.logo.url as string}
+                  src={item.logo.url}
                 />
               </Link>
-            );
-          }
-          return 'Increase the Depth';
-        })}
+            )
+        )}
+
+        {isUnpopulated && <UnpopulatedRelationshipFieldAlert />}
       </div>
     </ContentSection>
   );
