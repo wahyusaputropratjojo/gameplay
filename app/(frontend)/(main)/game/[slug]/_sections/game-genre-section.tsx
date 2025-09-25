e5ee60011cf21358ea76ae3a16234b0191d74e21
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-25 19:45:52 +0800

    feat(section): implement unpopulated relationship field alert component in game genre section

--- app/(frontend)/(main)/game/[slug]/_sections/game-genre-section.tsx
+++ app/(frontend)/(main)/game/[slug]/_sections/game-genre-section.tsx
@@ -1,8 +1,11 @@
+import { UnpopulatedRelationshipFieldAlert } from '@/components/alert/unpopulated-relationship-field-alert';
 import { ContentSection } from '@/components/content-section';
 import { Badge } from '@/components/ui/badge';
 import type { Game } from '@/lib/types/payload';
 
 export function GameGenreSection({ genre }: Pick<Game, 'genre'>) {
+  const isUnpopulated = genre.some((item) => typeof item === 'string');
+
   return (
     <ContentSection
       title="Genre"
@@ -10,12 +13,13 @@ export function GameGenreSection({ genre }: Pick<Game, 'genre'>) {
       titleSize="heading-4"
     >
       <div className="flex flex-wrap gap-2">
-        {genre.map((item) => {
-          if (typeof item === 'object') {
+        {genre
+          .filter((item) => typeof item === 'object')
+          .map((item) => {
             return <Badge key={item.id}>{item.name}</Badge>;
-          }
-          return 'Increase the Depth';
-        })}
+          })}
+
+        {isUnpopulated && <UnpopulatedRelationshipFieldAlert />}
       </div>
     </ContentSection>
   );
