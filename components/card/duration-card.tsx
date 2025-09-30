189ecbc0c4b7c96181a4bbd8c8d46aebe2c9e42e
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-09-30 16:45:15 +0800

    feat(component): update duration card style

--- components/card/duration-card.tsx
+++ components/card/duration-card.tsx
@@ -13,11 +13,11 @@ export function DurationCard({
 }: DurationCardProps) {
   return (
     <div className="flex items-center gap-4 rounded-(--card-radius) bg-gray-100 p-(--card-padding) [--card-padding:--spacing(1.5)] [--card-radius:var(--radius-xl)]">
-      <div className="flex size-18 shrink-0 flex-col items-center justify-center overflow-hidden rounded-[calc(var(--card-radius)-var(--card-padding))] bg-light text-gray-100">
-        <p className="select-none font-semibold text-xl">
+      <div className="flex size-20 shrink-0 flex-col items-center justify-center overflow-hidden rounded-[calc(var(--card-radius)-var(--card-padding))] bg-gray-200 text-gray-100">
+        <p className="select-none font-semibold text-gray-1000 text-xl">
           {numberToUnicodeFraction(duration)}
         </p>
-        <p className="select-none rounded-full bg-gray-100 px-2 text-center font-semibold text-gray-1000 text-xs">
+        <p className="select-none rounded-full bg-gray-1000 px-2 text-center font-semibold text-gray-100 text-xs">
           Hours
         </p>
       </div>
