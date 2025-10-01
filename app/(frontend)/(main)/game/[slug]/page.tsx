9e793dc180d5a404f5eb81c419c9a81d98be6070
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-10-01 22:31:25 +0800

    feat(page): update game page layout

--- app/(frontend)/(main)/game/[slug]/page.tsx
+++ app/(frontend)/(main)/game/[slug]/page.tsx
@@ -4,8 +4,8 @@ import { getGame } from '@/lib/queries/get-game';
 import { GameDescriptionSection } from './_sections/game-description-section';
 import { GameDeveloperSection } from './_sections/game-developer-section';
 import { GameDurationSection } from './_sections/game-duration-section';
-import { GameGenreSection } from './_sections/game-genre-section';
 import { GameImageSection } from './_sections/game-image-section';
+import { GameMetadataSection } from './_sections/game-metadata-section';
 import { GamePublisherSection } from './_sections/game-publisher-section';
 import { GameStoreSection } from './_sections/game-store-section';
 
@@ -33,8 +33,11 @@ export default async function Page({ params }: Props) {
 
   return (
     <article className="space-y-6">
-      <h1 className="lg:display text-pretty">{game.name}</h1>
-      <GameGenreSection genre={game.genre} />
+      <GameMetadataSection
+        genre={game.genre}
+        name={game.name}
+        theme={game.theme}
+      />
       <GameDescriptionSection description={game.description} />
       <GameImageSection hero={game.hero} logo={game.logo} />
       <div className="grid gap-8 md:grid-cols-2 md:gap-4">
