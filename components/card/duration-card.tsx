3d4015677598d643e38466a5b2571a90fcc468a9
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-10-03 17:26:11 +0800

    feat(component): implement main card component into duration card component

--- components/card/duration-card.tsx
+++ components/card/duration-card.tsx
@@ -1,3 +1,10 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { numberToUnicodeFraction } from '@/lib/utils/number-to-unicode-fraction';
 
 type DurationCardProps = {
@@ -12,19 +19,19 @@ export function DurationCard({
   description,
 }: DurationCardProps) {
   return (
-    <div className="flex items-center gap-4 rounded-(--card-radius) bg-gray-100 p-(--card-padding) [--card-padding:--spacing(1.5)] [--card-radius:var(--radius-xl)]">
-      <div className="flex size-20 shrink-0 flex-col items-center justify-center overflow-hidden rounded-[calc(var(--card-radius)-var(--card-padding))] bg-gray-200 text-gray-100">
+    <Card className="items-center" orientation="horizontal">
+      <CardContent className="flex size-20 shrink-0 flex-col items-center justify-center overflow-hidden rounded-[calc(var(--card-radius)-var(--card-padding))] bg-gray-200 text-gray-100">
         <p className="select-none font-semibold text-gray-1000 text-xl">
           {numberToUnicodeFraction(duration)}
         </p>
         <p className="select-none rounded-full bg-gray-1000 px-2 text-center font-semibold text-gray-100 text-xs">
           Hours
         </p>
-      </div>
-      <div>
-        <p className="font-medium">{title}</p>
-        <p className="font-light text-xs">{description}</p>
-      </div>
-    </div>
+      </CardContent>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+    </Card>
   );
 }
