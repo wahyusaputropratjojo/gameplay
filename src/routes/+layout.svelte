ded7739599515018c0e8bf444bfe89f77582ebe6
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-03-29 16:08:50 +0800

    feat(layout): implement theme in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { ModeWatcher } from "mode-watcher";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher track={false} />
 {@render children()}
