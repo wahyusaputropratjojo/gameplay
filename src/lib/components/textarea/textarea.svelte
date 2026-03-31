bbb16c5a4c31d9c37a43a01027c82d6104ece2e6
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-03-31 18:23:27 +0800

    feat(component): create textarea component

---
+++ src/lib/components/textarea/textarea.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import type { HTMLTextareaAttributes } from "svelte/elements";
+
+  type TextareaProps = HTMLTextareaAttributes & {
+    ref?: HTMLTextAreaElement | null;
+  };
+
+  let {
+    ref = $bindable(null),
+    value = $bindable(),
+    class: className,
+    ...props
+  }: TextareaProps = $props();
+</script>
+
+<textarea bind:this={ref} class={["textarea", className]} bind:value {...props}
+></textarea>
+
+<style>
+  :global {
+    @layer components {
+      .textarea {
+        outline-color: transparent;
+        outline-style: solid;
+        outline-width: 3px;
+        outline-offset: 1px;
+        border-width: 1px;
+        border-style: solid;
+        border-color: var(--color-neutral-6);
+        border-radius: 0.5em;
+        background-color: var(--color-neutral-1);
+        padding-inline: 0.75rem;
+        padding-block: 0.5rem;
+        min-height: 7.5rem;
+        font-size: var(--body-medium);
+        line-height: 1.5;
+        letter-spacing: normal;
+
+        &:focus-visible {
+          outline-color: var(--color-primary-8);
+
+          &[aria-invalid="true"] {
+            outline-color: var(--color-danger-8);
+          }
+        }
+
+        &[aria-invalid="true"] {
+          border-color: var(--color-danger-6);
+          background-color: var(--color-danger-1);
+        }
+      }
+    }
+  }
+</style>
